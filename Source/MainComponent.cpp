231bfc4e40662ac9c5b3f27e5da77c3bb7a3488d
hsstraub <straub@datacomm.ch>
2019-01-20 13:04:27 +0100

    Creation and resizing of maincomponent's elements separated

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -17,69 +17,38 @@
 MainContentComponent::MainContentComponent()
 : currentSetSelection(-1), currentSingleKeySelection(-1)
 {
-	setSize(DEFAULTMAINWINDOWWIDTH, DEFAULTMAINWINDOWHEIGHT);
-
 	// Key set fields
 	for (int i = 0; i < NUMBEROFBOARDS; i++)
 	{
 		// Macro button
 		macroButtons[i] = new TerpstraMacroButton();
 		addAndMakeVisible(macroButtons[i]);
-		macroButtons[i]->setBounds(FIRSTMACROBUTTOONCOLPOS + i*DEFAULTTERPSTRAKEYSETWIDTH, MACROBUTTONYPOS, macroButtons[i]->getWidth(), macroButtons[i]->getHeight());
 
 		// Paint set fields from right to left
 		// (This will not matter any more when the images' backgrounds are transparent)
 		// Width and heigth: were taken from image
 		terpstraSetSelectors[4-i] = new TerpstraKeySetEdit();
 		addAndMakeVisible(terpstraSetSelectors[4-i]);
-		terpstraSetSelectors[4 - i]->setBounds(MAINWINDOWFIRSTCOLPOS + (4 - i)*DEFAULTTERPSTRAKEYSETWIDTH, TERPSTRAKEYSETFLDFIRSTYPOS, terpstraSetSelectors[4 - i]->getWidth(), terpstraSetSelectors[4 - i]->getHeight());
 		terpstraSetSelectors[4-i]->addListener(this);
 	}
 
 	// Single Key fields
 
-	// Transformation Rotate slightly counterclockwise
-	int x = MAINWINDOWFIRSTCOLPOS;
-	int y = TERPSTRASINGLEKEYFIELDFIRSTYPOS;
-	AffineTransform transform = AffineTransform::translation(-x, -y);
-	transform = transform.rotated(TERPSTRASINGLEKEYROTATIONANGLE);
-	transform = transform.translated(x, y);
-
 	// Rows from right to left
 	// first row
-	x = MAINWINDOWFIRSTCOLPOS;
-	y = TERPSTRASINGLEKEYFIELDFIRSTYPOS;
-	transform.transformPoint(x, y);
 	terpstraKeyFields[0] = new TerpstraKeyEdit();
 	addAndMakeVisible(terpstraKeyFields[0]);
-	terpstraKeyFields[0]->setBounds(x, y, TERPSTRASINGLEKEYFLDSIZE, TERPSTRASINGLEKEYFLDSIZE);
 
 	terpstraKeyFields[1] = new TerpstraKeyEdit();
 	addAndMakeVisible(terpstraKeyFields[1]);
-	x = MAINWINDOWFIRSTCOLPOS + TERPSTRASINGLEKEYFLDSIZE;
-	y = TERPSTRASINGLEKEYFIELDFIRSTYPOS;
-	transform.transformPoint(x, y);
-	terpstraKeyFields[1]->setBounds(x, y, TERPSTRASINGLEKEYFLDSIZE, TERPSTRASINGLEKEYFLDSIZE);
 
 	// 8 identical rows
 	for (int row = 0; row < 8; row++)
 	{
-		int xbasepos;
-		if (row % 2 == 0)
-			xbasepos = MAINWINDOWFIRSTCOLPOS + TERPSTRASINGLEKEYFLDSIZE / 2;
-		else
-			xbasepos = MAINWINDOWFIRSTCOLPOS;
-
-		int ybasepos = TERPSTRASINGLEKEYFIELDFIRSTYPOS + 3 * (row + 1) * TERPSTRASINGLEKEYFLDSIZE / 4;
-
 		for (int i = 0; i < 6; i++)
 		{
 			terpstraKeyFields[2 + 6 * row + i] = new TerpstraKeyEdit();
 			addAndMakeVisible(terpstraKeyFields[2 + 6 * row + i]);
-			x = xbasepos + i*TERPSTRASINGLEKEYFLDSIZE;
-			y = ybasepos;
-			transform.transformPoint(x, y);
-			terpstraKeyFields[2 + 6 * row + i]->setBounds(x, y, TERPSTRASINGLEKEYFLDSIZE, TERPSTRASINGLEKEYFLDSIZE);
 		}
 	}
 
@@ -88,19 +57,11 @@ MainContentComponent::MainContentComponent()
 	{
 		terpstraKeyFields[50 + i] = new TerpstraKeyEdit();
 		addAndMakeVisible(terpstraKeyFields[50 + i]);
-		x = MAINWINDOWFIRSTCOLPOS + (2 * i + 3)*TERPSTRASINGLEKEYFLDSIZE / 2;
-		y = TERPSTRASINGLEKEYFIELDFIRSTYPOS + 3 * 9 * TERPSTRASINGLEKEYFLDSIZE / 4;
-		transform.transformPoint(x, y);
-		terpstraKeyFields[50 + i]->setBounds(x, y, TERPSTRASINGLEKEYFLDSIZE, TERPSTRASINGLEKEYFLDSIZE);
 	}
 
 	// 10th row
 	terpstraKeyFields[55] = new TerpstraKeyEdit();
 	addAndMakeVisible(terpstraKeyFields[55]);
-	x = MAINWINDOWFIRSTCOLPOS + 5 * TERPSTRASINGLEKEYFLDSIZE;
-	y = TERPSTRASINGLEKEYFIELDFIRSTYPOS + 3 * 10 * TERPSTRASINGLEKEYFLDSIZE / 4;
-	transform.transformPoint(x, y);
-	terpstraKeyFields[55]->setBounds(x, y, TERPSTRASINGLEKEYFLDSIZE, TERPSTRASINGLEKEYFLDSIZE);
 
 	for (int i = 0; i < TERPSTRABOARDSIZE; i++)
 		terpstraKeyFields[i]->addMouseListener(this, true);
@@ -108,12 +69,14 @@ MainContentComponent::MainContentComponent()
 	// Midi input + output
 	midiEditArea = new MidiEditArea();
 	addAndMakeVisible(midiEditArea);
-	midiEditArea->setBounds(EDITAREAFIRSTCOLPOS, MIDIEDITAREAFIRSTROWPOS, EDITAREAWIDTH, MIDIEDITAREAHEIGHT);
 	midiEditArea->addSendAllButtonListener(this);
 
+	// Edit function area
 	noteEditArea = new NoteEditArea();
 	addAndMakeVisible(noteEditArea);
-	noteEditArea->setBounds(EDITAREAFIRSTCOLPOS, MIDIEDITAREAFIRSTROWPOS + MIDIEDITAREAHEIGHT, EDITAREAWIDTH, EDITFUNCTIONAREAHEIGHT);
+
+	// Initial size 
+	setSize(DEFAULTMAINWINDOWWIDTH, DEFAULTMAINWINDOWHEIGHT);
 
 	// Select first board and first key
 	changeSetSelection(0);
@@ -192,6 +155,80 @@ void MainContentComponent::resized()
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+
+	// Key set fields
+	for (int i = 0; i < NUMBEROFBOARDS; i++)
+	{
+		// Macro button
+		macroButtons[i]->setBounds(FIRSTMACROBUTTOONCOLPOS + i*DEFAULTTERPSTRAKEYSETWIDTH, MACROBUTTONYPOS, macroButtons[i]->getWidth(), macroButtons[i]->getHeight());
+
+		// Paint set fields from right to left
+		// (This will not matter any more when the images' backgrounds are transparent)
+		// Width and heigth: were taken from image
+		terpstraSetSelectors[4 - i]->setBounds(MAINWINDOWFIRSTCOLPOS + (4 - i)*DEFAULTTERPSTRAKEYSETWIDTH, TERPSTRAKEYSETFLDFIRSTYPOS, terpstraSetSelectors[4 - i]->getWidth(), terpstraSetSelectors[4 - i]->getHeight());
+	}
+
+	// Single Key fields
+
+	// Transformation Rotate slightly counterclockwise
+	int x = MAINWINDOWFIRSTCOLPOS;
+	int y = TERPSTRASINGLEKEYFIELDFIRSTYPOS;
+	AffineTransform transform = AffineTransform::translation(-x, -y);
+	transform = transform.rotated(TERPSTRASINGLEKEYROTATIONANGLE);
+	transform = transform.translated(x, y);
+
+	// Rows from right to left
+	// first row
+	x = MAINWINDOWFIRSTCOLPOS;
+	y = TERPSTRASINGLEKEYFIELDFIRSTYPOS;
+	transform.transformPoint(x, y);
+	terpstraKeyFields[0]->setBounds(x, y, TERPSTRASINGLEKEYFLDSIZE, TERPSTRASINGLEKEYFLDSIZE);
+
+	x = MAINWINDOWFIRSTCOLPOS + TERPSTRASINGLEKEYFLDSIZE;
+	y = TERPSTRASINGLEKEYFIELDFIRSTYPOS;
+	transform.transformPoint(x, y);
+	terpstraKeyFields[1]->setBounds(x, y, TERPSTRASINGLEKEYFLDSIZE, TERPSTRASINGLEKEYFLDSIZE);
+
+	// 8 identical rows
+	for (int row = 0; row < 8; row++)
+	{
+		int xbasepos;
+		if (row % 2 == 0)
+			xbasepos = MAINWINDOWFIRSTCOLPOS + TERPSTRASINGLEKEYFLDSIZE / 2;
+		else
+			xbasepos = MAINWINDOWFIRSTCOLPOS;
+
+		int ybasepos = TERPSTRASINGLEKEYFIELDFIRSTYPOS + 3 * (row + 1) * TERPSTRASINGLEKEYFLDSIZE / 4;
+
+		for (int i = 0; i < 6; i++)
+		{
+			x = xbasepos + i*TERPSTRASINGLEKEYFLDSIZE;
+			y = ybasepos;
+			transform.transformPoint(x, y);
+			terpstraKeyFields[2 + 6 * row + i]->setBounds(x, y, TERPSTRASINGLEKEYFLDSIZE, TERPSTRASINGLEKEYFLDSIZE);
+		}
+	}
+
+	// 9th row
+	for (int i = 0; i < 5; i++)
+	{
+		x = MAINWINDOWFIRSTCOLPOS + (2 * i + 3)*TERPSTRASINGLEKEYFLDSIZE / 2;
+		y = TERPSTRASINGLEKEYFIELDFIRSTYPOS + 3 * 9 * TERPSTRASINGLEKEYFLDSIZE / 4;
+		transform.transformPoint(x, y);
+		terpstraKeyFields[50 + i]->setBounds(x, y, TERPSTRASINGLEKEYFLDSIZE, TERPSTRASINGLEKEYFLDSIZE);
+	}
+
+	// 10th row
+	x = MAINWINDOWFIRSTCOLPOS + 5 * TERPSTRASINGLEKEYFLDSIZE;
+	y = TERPSTRASINGLEKEYFIELDFIRSTYPOS + 3 * 10 * TERPSTRASINGLEKEYFLDSIZE / 4;
+	transform.transformPoint(x, y);
+	terpstraKeyFields[55]->setBounds(x, y, TERPSTRASINGLEKEYFLDSIZE, TERPSTRASINGLEKEYFLDSIZE);
+
+	// Midi input + output
+	midiEditArea->setBounds(EDITAREAFIRSTCOLPOS, MIDIEDITAREAFIRSTROWPOS, EDITAREAWIDTH, MIDIEDITAREAHEIGHT);
+
+	// Edit function area
+	noteEditArea->setBounds(EDITAREAFIRSTCOLPOS, MIDIEDITAREAFIRSTROWPOS + MIDIEDITAREAHEIGHT, EDITAREAWIDTH, EDITFUNCTIONAREAHEIGHT);
 }
 
 void MainContentComponent::buttonClicked(Button *button)
