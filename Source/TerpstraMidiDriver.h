643928858cc499f64981e45abfaded618f5126ed
hsstraub <straub@datacomm.ch>
2019-03-16 12:59:49 +0100

    Lowercase/Uppercase correction for Linux. Program icon for MacOS.

--- Source/TerpstraMidiDriver.h
+++ Source/TerpstraMidiDriver.h
@@ -14,7 +14,7 @@
 //[Headers]     -- You can add your own extra header files here --
 #include "JuceHeader.h"
 #include "KeyboardDataStructure.h"
-#include "Hajulib/HajuDelayMidiDriver.h"
+#include "HajuLib/HajuDelayMidiDriver.h"
 //[/Headers]
 
 /*
