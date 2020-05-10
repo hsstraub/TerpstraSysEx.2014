add9edb6ad28f74ee4781aaddfe8cdc0f6454dcb
hsstraub <straub@datacomm.ch>
2020-05-10 21:08:05 +0200

    JUCE 5.4.7, with compile flags for CodeBlocks 20.3/MinGW

--- Source/IsomorphicMassAssign.h
+++ Source/IsomorphicMassAssign.h
@@ -7,7 +7,7 @@
   the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
   and re-saved.
 
-  Created with Projucer version: 5.4.5
+  Created with Projucer version: 5.4.7
 
   ------------------------------------------------------------------------------
 
@@ -44,7 +44,7 @@ class IsomorphicMassAssign  : public Component,
 public:
     //==============================================================================
     IsomorphicMassAssign ();
-    ~IsomorphicMassAssign();
+    ~IsomorphicMassAssign() override;
 
     //==============================================================================
     //[UserMethods]     -- You can add your own custom methods in this section.
