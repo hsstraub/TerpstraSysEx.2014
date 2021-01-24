e1e046cee558ede167e8d1a99c9e3ee5b66fabfa
hsstraub <straub@datacomm.ch>
2021-01-24 22:19:19 +0100

    Default aftertouch velocity curve

--- Source/KeyboardDataStructure.cpp
+++ Source/KeyboardDataStructure.cpp
@@ -55,6 +55,12 @@ int DefaultFaderVelocityTable[128] = {
 	34, 35, 35, 36, 37, 37, 38, 39, 39, 40, 41, 41, 42, 43, 44, 45, 45, 46, 47, 48, 49, 50, 51, 52, 53, 55, 56, 57, 59, 62, 65, 68, 71, 74, 77,
 	79, 82, 85, 88, 91, 94, 97, 99, 102, 105, 108, 111, 114, 117, 119, 122, 125, 127 };
 
+int DefaultAfterTouchVelocityTable[128] = {
+	0, 2, 3, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, 31, 32, 33, 34, 36, 37, 38, 39, 40, 41, 43, 44, 45,
+	46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+	81, 82, 83, 84 , 85, 85, 86, 87, 88, 89, 90, 91, 92, 92, 93, 94, 95, 96, 97, 98, 99, 99, 100, 101, 102, 103, 104, 104, 105, 106, 107, 108,
+	109, 110, 111, 112, 112, 113, 114, 115, 116, 116, 117, 118, 119, 120, 120, 121, 122, 123, 123, 124, 125, 126, 126, 127 };
+
 TerpstraVelocityCurveConfig::TerpstraVelocityCurveConfig()
 {
 	// Default config: one to one
@@ -75,7 +81,10 @@ TerpstraVelocityCurveConfig::TerpstraVelocityCurveConfig(TerpstraVelocityCurveCo
 		break;
 
 	case TerpstraVelocityCurveConfig::VelocityCurveType::afterTouch:
-		// ToDo
+		jassert(sizeof(DefaultAfterTouchVelocityTable) == sizeof(velocityValues));
+		memmove(velocityValues, DefaultAfterTouchVelocityTable, sizeof(DefaultAfterTouchVelocityTable));
+		editStrategy = EDITSTRATEGYINDEX::freeDrawing;
+		break;
 
 	case TerpstraVelocityCurveConfig::VelocityCurveType::noteOnNoteOff:
 	default:
