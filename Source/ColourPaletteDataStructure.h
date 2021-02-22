a371dc192d177eda74b33786f0c430f5049b6cbe
Vincenzo Sicurella <sicurella12@gmail.com>
2021-02-22 00:01:40 -0500

    Added properties and file handling to palettes

--- Source/ColourPaletteDataStructure.h
+++ Source/ColourPaletteDataStructure.h
@@ -11,17 +11,17 @@
 #pragma once
 #include <JuceHeader.h>
 
-#define COLOURPALETTESIZE 6
-#define STRINGCOLOURSIZE 8
-
-
 namespace LumatoneEditorPaletteIds
 {
-    static Identifier libraryId = Identifier("PaletteLibrary");
-    static Identifier paletteId = Identifier("ColourPalette");
-    static Identifier swatchId = Identifier("Swatches");
-    static Identifier colourId = Identifier("Colour");
-    static Identifier nameId = Identifier("Name");
+    static Identifier libraryId         = Identifier("PaletteLibrary");
+    static Identifier paletteId         = Identifier("ColourPalette");
+    static Identifier swatchId          = Identifier("Swatch");
+    static Identifier colourId          = Identifier("Colour");
+
+    static Identifier nameId            = Identifier("Name");
+    static Identifier dateCreatedId     = Identifier("DateCreated");
+    static Identifier authorId          = Identifier("Author");
+    static Identifier notesId           = Identifier("Notes");
 }
 
 //==============================================================================
@@ -30,34 +30,34 @@ namespace LumatoneEditorPaletteIds
 */
 struct LumatoneEditorColourPalette
 {
+
     LumatoneEditorColourPalette(String paletteName="")
     {
         colourPalette.reset(new Array<Colour>());
         palette = colourPalette.get();
         name = paletteName;
+        dateCreated = Time::getCurrentTime().toISO8601(false);
     }
 
     LumatoneEditorColourPalette(Array<Colour> colourPaletteIn, String paletteName="")
         : LumatoneEditorColourPalette(paletteName)
     {
-        for (auto colour : colourPaletteIn)
-            palette->add(colour);
+        palette->clear();
+        palette->addArray(colourPaletteIn);
     }
 
-    LumatoneEditorColourPalette(ValueTree paletteNode)
-        : LumatoneEditorColourPalette()
+    LumatoneEditorColourPalette(Array<Colour> colourPaletteIn, String paletteName, String paletteAuthor, String paletteNotes="")
+        : LumatoneEditorColourPalette(colourPaletteIn, paletteName)
     {
-        name = paletteNode[LumatoneEditorPaletteIds::nameId];
+        name = paletteName;
+        author = paletteAuthor;
+        notes = paletteNotes;
 
-        for (auto swatch : paletteNode)
-        {
-            if (swatch.hasType(LumatoneEditorPaletteIds::swatchId))
-                palette->add(Colour::fromString(swatch[LumatoneEditorPaletteIds::colourId].toString()));
-        }
+        palette->clear();
+        palette->addArray(colourPaletteIn);
     }
 
-    LumatoneEditorColourPalette(const LumatoneEditorColourPalette& palette)
-        : LumatoneEditorColourPalette(*palette.palette, palette.name) {}
+    LumatoneEditorColourPalette(const LumatoneEditorColourPalette& paletteToCopy) : LumatoneEditorColourPalette() { *this = paletteToCopy; }
 
     ~LumatoneEditorColourPalette()
     {
@@ -65,12 +65,32 @@ struct LumatoneEditorColourPalette
         colourPalette = nullptr;
     }
 
+    void operator=(const LumatoneEditorColourPalette& paletteToCopy)
+    {
+        this->palette->clear();
+        this->palette->addArray(*paletteToCopy.palette);
+
+        this->name          = paletteToCopy.name;
+        this->author        = paletteToCopy.author;
+        this->notes         = paletteToCopy.notes;
+        this->dateCreated   = paletteToCopy.dateCreated;
+
+        this->pathToFile    = paletteToCopy.pathToFile;
+    }
+
     int size() const { return palette->size(); }
+
+    String getPathToFile() const { return pathToFile; }
+
+    void setPathToFile(String newPathToFile) { pathToFile = newPathToFile; }
 
     ValueTree toValueTree() const
     {
         ValueTree node(LumatoneEditorPaletteIds::paletteId);
         node.setProperty(LumatoneEditorPaletteIds::nameId, name, nullptr);
+        node.setProperty(LumatoneEditorPaletteIds::authorId, author, nullptr);
+        node.setProperty(LumatoneEditorPaletteIds::dateCreatedId, dateCreated, nullptr);
+        node.setProperty(LumatoneEditorPaletteIds::notesId, notes, nullptr);
 
         for (auto c : *colourPalette)
         {
@@ -92,71 +112,98 @@ struct LumatoneEditorColourPalette
 
     Array<Colour>* palette;
     String name = "";
+    String author = "";
+    String notes = "";
 
     //==============================================================================
+    // Palette static methods
+
+    static LumatoneEditorColourPalette loadFromValueTree(ValueTree paletteNode)
+    {
+        LumatoneEditorColourPalette loadedPalette;
+
+        if (paletteNode.hasType(LumatoneEditorPaletteIds::paletteId))
+        {
+            Array<Colour> palette;
+
+            for (auto swatch : paletteNode)
+            {
+                if (swatch.hasType(LumatoneEditorPaletteIds::swatchId))
+                    palette.add(Colour::fromString(swatch[LumatoneEditorPaletteIds::colourId].toString()));
+            }
+
+            loadedPalette = LumatoneEditorColourPalette(palette,
+                paletteNode[LumatoneEditorPaletteIds::nameId],
+                paletteNode[LumatoneEditorPaletteIds::authorId],
+                paletteNode[LumatoneEditorPaletteIds::notesId]
+            );
+
+            if (paletteNode[LumatoneEditorPaletteIds::dateCreatedId].toString() != String())
+                loadedPalette.dateCreated = paletteNode[LumatoneEditorPaletteIds::dateCreatedId];
+        }
+
+        return loadedPalette;
+    }
+
+    static LumatoneEditorColourPalette loadFromFile(const File& pathToPalette)
+    {
+        LumatoneEditorColourPalette loadedPalette;
+
+        if (pathToPalette.existsAsFile())
+        {
+            loadedPalette = loadFromValueTree(ValueTree::fromXml(pathToPalette.loadFileAsString()));
+
+            // Set empty palette name to file name if it does not appear to be a placeholder
+            if (loadedPalette.name == String() && !pathToPalette.getFileNameWithoutExtension().contains("UnnamedPalette"))
+            {
+                loadedPalette.name = pathToPalette.getFileNameWithoutExtension();
+            }
+        }
+
+        loadedPalette.pathToFile = pathToPalette.getFullPathName();
+
+        return loadedPalette;
+    }
+
+    //==============================================================================
+    // Palette Library based static methods, currently unused in favor of separate palette files
+
+    //static String paletteArrayToString(const Array<LumatoneEditorColourPalette>& palettes)
+    //{
+    //    ValueTree libraryNode(LumatoneEditorPaletteIds::libraryId);
+
+    //    for (int p = 0; p < palettes.size(); p++)
+    //    {
+    //        libraryNode.appendChild(palettes[p].toValueTree(), nullptr);
+    //    }
+
+    //    return libraryNode.toXmlString();
+    //}
+
+    //static Array<LumatoneEditorColourPalette> loadPalettesFromString(String stringIn)
+    //{
+    //    ValueTree libraryNode = ValueTree::fromXml(stringIn);
+    //    return loadPalettesFromNode(libraryNode);
+    //}
+
+    //static Array<LumatoneEditorColourPalette> loadPalettesFromNode(ValueTree nodeIn)
+    //{
+    //    Array<LumatoneEditorColourPalette> library;
+
+    //    if (nodeIn.hasType(LumatoneEditorPaletteIds::libraryId))
+    //    {
+    //        for (auto palette : nodeIn)
+    //            library.add(LumatoneEditorColourPalette(palette));
+    //    }
+
+    //    return library;
+    //}
+
+private:
     
-    static String paletteArrayToString(const Array<LumatoneEditorColourPalette>& palettes)
-    {
-        ValueTree libraryNode(LumatoneEditorPaletteIds::libraryId);
-
-        for (int p = 0; p < palettes.size(); p++)
-        {
-            libraryNode.appendChild(palettes[p].toValueTree(), nullptr);
-        }
-
-        return libraryNode.toXmlString();
-    }
-
-    static Array<LumatoneEditorColourPalette> loadPalettesFromString(String stringIn)
-    {
-        ValueTree libraryNode = ValueTree::fromXml(stringIn);
-
-        if (libraryNode.hasType(LumatoneEditorPaletteIds::libraryId))
-            return loadPalettesFromNode(libraryNode);
-        
-        else if (!libraryNode.isValid())
-            return loadPalettesFromStringOld(stringIn);
-    }
-
-    static Array<LumatoneEditorColourPalette> loadPalettesFromNode(ValueTree nodeIn)
-    {
-        Array<LumatoneEditorColourPalette> library;
-
-        if (nodeIn.hasType(LumatoneEditorPaletteIds::libraryId))
-        {
-            for (auto palette : nodeIn)
-                library.add(LumatoneEditorColourPalette(palette));
-        }
-
-        return library;
-    }
-
-private:
-
-    // Palettes should be separated by a space
-    static Array<LumatoneEditorColourPalette> loadPalettesFromStringOld(String stringIn)
-    {
-        Array<LumatoneEditorColourPalette> palettesOut;
-
-        int i = 0;
-        while (i < stringIn.length())
-        {
-            int endIndex = stringIn.indexOf(i, " ");
-            if (endIndex < 0)
-                endIndex = stringIn.length();
-
-            auto paletteString = stringIn.substring(i, endIndex);
-            if (paletteString.length() > 0)
-                palettesOut.add(LumatoneEditorColourPalette(stringIn.substring(i, endIndex)));
-
-            i += paletteString.length() + 1;
-        }
-
-        return palettesOut;
-    }
-
-private:
-
     std::unique_ptr<Array<Colour>> colourPalette;
+    String dateCreated = "";
 
+    // Internal use only
+    String pathToFile = "";
 };
