07aa1dea28fec3e06484e6ad17cd755244381ba4
Vincenzo Sicurella <sicurella12@gmail.com>
2021-07-06 21:23:40 -0400

    Updated firmware versioning

--- Source/LumatoneFirmwareDefinitions.h
+++ Source/LumatoneFirmwareDefinitions.h
@@ -146,20 +146,17 @@ System exclusive command bytes
 #define CALLIBRATE_EXPRESSION_PEDAL 0x38
 #define RESET_EXPRESSION_PEDAL_BOUNDS 0x39
 
-// Firmware Version 1.0.12
+// Firmware Version 1.0.12 / 1.1.0
 #define GET_BOARD_THRESHOLD_VALUES 0x3A
 #define GET_BOARD_SENSITIVITY_VALUES 0x3B
 
-// Firmware Version 1.0.13
 #define SET_PERIPHERAL_CHANNELS 0x3C
 #define GET_PERIPHERAL_CHANNELS 0x3D
 #define PERIPHERAL_CALBRATION_DATA 0x3E
 
-// Firmware Version 1.0.14
 #define SET_AFTERTOUCH_TRIGGER_DELAY 0x3F
 #define GET_AFTERTOUCH_TRIGGER_DELAY 0x40
 
-// Firmware Version 1.0.15
 #define SET_LUMATOUCH_NOTE_OFF_DELAY 0x41
 #define GET_LUMATOUCH_NOTE_OFF_DELAY 0x42
 #define SET_EXPRESSION_PEDAL_THRESHOLD 0x43
@@ -200,10 +197,8 @@ enum class LumatoneFirmwareVersion
 	VERSION_1_0_10,
 	VERSION_1_0_11,
 	VERSION_1_0_12,
-	VERSION_1_0_13,
-	VERSION_1_0_14,
-	VERSION_1_0_15,
-	LAST_VERSION = VERSION_1_0_15,
+    VERSION_1_1_0 = VERSION_1_0_12,
+	LAST_VERSION = VERSION_1_1_0,
 	FUTURE_VERSION = 0xFFFF  // Used when version is nonnegative and below 9.9.999
 } ;
 
@@ -346,8 +341,10 @@ struct FirmwareSupport
 		else if ((versionIn.major < 0) | (versionIn.minor < 0) | (versionIn.revision < 0))
 			return LumatoneFirmwareVersion::UNKNOWN_VERSION;
 		
+        // MAJOR: 1
 		else if (versionIn.major == 1)
 		{
+            // MINOR: 0
 			if (versionIn.minor == 0)
 			{
 				if (versionIn.revision < 3)
@@ -359,6 +356,15 @@ struct FirmwareSupport
 				else if (versionIn.revision >= 3)
 					return (LumatoneFirmwareVersion)((int)LumatoneFirmwareVersion::VERSION_1_0_3 + (versionIn.revision - 3));
 			}
+            
+            // MINOR: 1
+            else if (versionIn.minor == 1)
+            {
+                if (versionIn.revision == 0)
+                    return LumatoneFirmwareVersion::VERSION_1_1_0;
+                else
+                    return LumatoneFirmwareVersion::FUTURE_VERSION;
+            }
 		}
 
 		else if (versionIn.major < 9 && versionIn.minor < 9 && versionIn.revision < 999)
@@ -372,22 +378,22 @@ struct FirmwareSupport
 	LumatoneFirmwareVersion getLowestVersionAcknowledged(unsigned int CMD)
 	{
 		if (CMD < CHANGE_KEY_NOTE) // 0x00
-			LumatoneFirmwareVersion::UNKNOWN_VERSION;
+			return LumatoneFirmwareVersion::UNKNOWN_VERSION;
 
 		else if (CMD <= GET_SERIAL_IDENTITY) // 0x23
-			LumatoneFirmwareVersion::NO_VERSION;
+			return LumatoneFirmwareVersion::NO_VERSION;
 
 		else if (CMD <= DEMO_MODE) // 0x25
-			LumatoneFirmwareVersion::VERSION_1_0_5;
+			return LumatoneFirmwareVersion::VERSION_1_0_5;
 
 		else if (CMD <= CALIBRATE_PITCH_MOD_WHEEL) // 0x26
-			LumatoneFirmwareVersion::VERSION_1_0_6;
+			return LumatoneFirmwareVersion::VERSION_1_0_6;
 
 		else if (CMD <= SET_KEY_MAX_THRESHOLD) // 0x29
-			LumatoneFirmwareVersion::VERSION_1_0_7;
+			return LumatoneFirmwareVersion::VERSION_1_0_7;
 
 		else if (CMD <= GET_FIRMWARE_REVISION) // 0x31
-			LumatoneFirmwareVersion::VERSION_1_0_8;
+			return LumatoneFirmwareVersion::VERSION_1_0_8;
 
 		else if (CMD <= LUMA_PING) // 0x33
 			return LumatoneFirmwareVersion::VERSION_1_0_9;
@@ -398,17 +404,8 @@ struct FirmwareSupport
 		else if (CMD <= RESET_EXPRESSION_PEDAL_BOUNDS) // 0x39
 			return LumatoneFirmwareVersion::VERSION_1_0_11;
 
-		else if (CMD <= GET_BOARD_SENSITIVITY_VALUES) // 0x3B
-			return LumatoneFirmwareVersion::VERSION_1_0_12;
-
-		else if (CMD <= PERIPHERAL_CALBRATION_DATA) // 0x3E
-			return LumatoneFirmwareVersion::VERSION_1_0_13;
-
-		else if (CMD <= SET_AFTERTOUCH_TRIGGER_DELAY) // 0x3F
-			return LumatoneFirmwareVersion::VERSION_1_0_14;
-
-		else if (CMD <= RESET_DEFAULT_PRESETS) //0x45
-			return LumatoneFirmwareVersion::VERSION_1_0_15;
+		else if (CMD <= RESET_DEFAULT_PRESETS) // 0x3B
+			return LumatoneFirmwareVersion::VERSION_1_1_0;
 
 		else
 			return LumatoneFirmwareVersion::FUTURE_VERSION;
