c0763ce3a35bb42924bba673972e26a5b682849d
Hans Straub <hans.straub@messerli.ch>
2024-05-05 19:21:01 +0200

    New positions for text edit and button in ProJucer code, too

--- Source/KBMForOneChannel.cpp
+++ Source/KBMForOneChannel.cpp
@@ -301,11 +301,11 @@ BEGIN_JUCER_METADATA
             virtualName="" explicitFocusOrder="0" pos="0 8 48 24" editable="0"
             layout="33" items="" textWhenNonSelected="" textWhenNoItems="(no choices)"/>
   <TEXTEDITOR name="textMappingFile" id="b9bc5baf677ed902" memberName="textMappingFile"
-              virtualName="" explicitFocusOrder="0" pos="56 8 104 24" initialText=""
+              virtualName="" explicitFocusOrder="0" pos="56 8 68 24" initialText=""
               multiline="0" retKeyStartsLine="0" readonly="0" scrollbars="1"
               caret="1" popupmenu="1"/>
   <TEXTBUTTON name="btnFileSelectMacro" id="23cc77cbad6653d7" memberName="btnFileSelectMacro"
-              virtualName="" explicitFocusOrder="0" pos="168 8 24 24" buttonText="..."
+              virtualName="" explicitFocusOrder="0" pos="132 8 24 24" buttonText="..."
               connectedEdges="0" needsCallback="1" radioGroupId="0"/>
 </JUCER_COMPONENT>
 
