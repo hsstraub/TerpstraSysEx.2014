7d9e79556ba2e408cda39e85c7e51314848cb3fe
Hans Straub <hans.straub@messerli.ch>
2024-05-20 11:22:40 +0200

    Edit control  for kbm file in varying size

--- Source/IsomorphicMassAssign.cpp
+++ Source/IsomorphicMassAssign.cpp
@@ -257,11 +257,11 @@ void IsomorphicMassAssign::paint (juce::Graphics& g)
 void IsomorphicMassAssign::resized()
 {
     //[UserPreResize] Add your own custom resize code here..
-	int width = getWidth();
+	int newSubDlgWidth = getWidth() - (2 * groupMapping->getX());
 
-    groupMapping->setBounds(groupMapping->getX(), groupMapping->getY(), width, groupMapping->getHeight());
-	incrMidiNotesMapping->setBounds(16, MAPPINGSUBWINTOP, width, MAPPINGSUBWINHEIGHT);
-	kbmMappingDlg->setBounds(16, MAPPINGSUBWINTOP, width, MAPPINGSUBWINHEIGHT);
+    groupMapping->setBounds(groupMapping->getX(), groupMapping->getY(), newSubDlgWidth, groupMapping->getHeight());
+	incrMidiNotesMapping->setBounds(16, MAPPINGSUBWINTOP, newSubDlgWidth, MAPPINGSUBWINHEIGHT);
+	kbmMappingDlg->setBounds(16, MAPPINGSUBWINTOP, newSubDlgWidth, MAPPINGSUBWINHEIGHT);
     //[/UserPreResize]
 
     //[UserResized] Add your own custom resize handling here..
